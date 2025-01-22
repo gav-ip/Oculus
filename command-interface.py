3cf6f5ec1cfb6c36f1d98d76c22c9946c511e9e1
Gavin Abrigo <135771042+gav-ip@users.noreply.github.com>
2025-01-22 00:47:32 -0800

    testing connection to etherum mainnet

--- command-interface.py
+++ command-interface.py
@@ -1,2 +1,13 @@
-import cmd
+import typer
 from web3 import Web3
+import requests
+
+#connect to an ethereum node using infura API
+infura_url = "https://mainnet.infura.io/v3/834f5b297d404c70a7f920c99450dc29"
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+#checking if connection is successful
+if web3.is_connected():
+    print("connected to Ethereum Mainnet")
+else:
+    print("Failed to connect")
